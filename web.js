f95d371d47fafa322e8cdcfa26a6610a0c7c57d8
Jamey McElveen <jamey.mcelveen@acstechnologies.com>
2014-02-03 01:07:24 -0500

    add session and route support

--- web.js
+++ web.js
@@ -1,15 +1,38 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require("express"),
+    logfmt = require("logfmt"),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path'),
+    app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+var sessionSecret =  process.env.SESSION_SECRET || '1234567890QWERTYUIOP';
 
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
+// all environments
+app.set('port', process.env.PORT || 29161);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser(sessionSecret));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
 });
